37a526dc89d3c8e08bde27a1724c6dbddd4d0376
Kazunyan <kazu@kazugmx.net>
2024-09-10 02:51:33 +0000

    upd transactions

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -16,20 +16,28 @@ export async function GET(req: NextRequest) {
         return NextResponse.json({ sum }, { status: 201 });
     }
     const transactions = await prisma.transaction.findMany();
-    return NextResponse.json({ transactions }, { status: 201 });
+    return NextResponse.json( {transactions} , { status: 202 });
 }
 
 // POSTメソッド
 export async function POST(req: NextRequest) {
-    let { description, amount, category, date} = await req.json();
-    amount = Number(amount);
-    const newTransaction = await prisma.transaction.create({
-        data: {
-            description,
-            amount,
-            category,
-            date: new Date(date),
-        },
-    });
-    return NextResponse.json(newTransaction, { status: 201 });
+    try {
+        let { description, amount, category, date} = await req.json();
+        if (!description || !amount || !category || !date) {
+            return NextResponse.json({ error: "Missing required fields" }, { status: 400 });
+        }
+        amount = Number(amount);
+        const newTransaction = await prisma.transaction.create({
+            data: {
+                description,
+                amount,
+                category,
+                date: new Date(date),
+            },
+        });
+        return NextResponse.json(newTransaction, { status: 201 });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: "Invalid JSON input" }, { status: 400 });
+    }
 }
